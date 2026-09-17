b60d33df6a4fe01726c5d4da0425a45b84cbf811
Ridwanul567 <ridwanulislam567@gmail.com>
2026-09-17 12:56:35 +0100

    Included AWS provider block in main

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,12 @@ terraform {
   }
 }
 
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 data "aws_vpc" "vpc" {
     id = var.vpc_id
 }
