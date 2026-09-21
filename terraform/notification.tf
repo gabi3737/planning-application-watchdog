fa19a04bb1520278d5ed647307f91e75ff10c239
Ridwanul567 <ridwanulislam567@gmail.com>
2026-09-21 12:40:35 +0100

    Added EventBridge Schedule for Lambda

--- terraform/notification.tf
+++ terraform/notification.tf
@@ -90,3 +90,83 @@ resource "aws_lambda_function" "notification_lambda" {
   memory_size = 256
   timeout     = 60
 }
+
+
+# Notification Lambda Schedule Role
+
+data "aws_iam_policy_document" "notification_schedule_trust_policy_doc" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["scheduler.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "notification_schedule_permissions_policy_doc" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["arn:aws:logs:eu-west-2:${var.account_id}:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    resources = [aws_lambda_function.notification_lambda.arn]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = [aws_iam_role.notification_schedule_role.arn]
+    condition {
+      test     = "StringLike"
+      variable = "iam:PassedToService"
+      values   = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "notification_schedule_role" {
+  name               = "c25-planning-notification-schedule-role"
+  assume_role_policy = data.aws_iam_policy_document.notification_schedule_trust_policy_doc.json
+}
+
+resource "aws_iam_policy" "notification_schedule_role_permissions_policy" {
+  name   = "c25-planning-notification-schedule-permissions-policy"
+  policy = data.aws_iam_policy_document.notification_schedule_permissions_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "notification_schedule_role_policy_connection" {
+  role       = aws_iam_role.notification_schedule_role.name
+  policy_arn = aws_iam_policy.notification_schedule_role_permissions_policy.arn
+}
+
+# EventBridge Schedule:
+
+resource "aws_scheduler_schedule" "notification-schedule" {
+  name                         = "c25-planning-notification-schedule"
+  group_name                   = "default"
+  schedule_expression          = "cron(0 8 * * ? *)"
+  schedule_expression_timezone = "Europe/London"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn      = aws_lambda_function.notification_lambda.arn
+    role_arn = aws_iam_role.notification_schedule_role.arn
+  }
+}
